355f602498f041b7a30db04fc9002d7fc149ea02
Jeremias Algonz <jeremiasgoncalez@gmail.com>
2022-02-24 22:25:54 -0300

    Implementada SplashScreen

---
+++ AwesomeApp/AwesomeApp.Android/SplashActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using System;
+using System.Threading.Tasks;
+
+
+namespace AwesomeApp.Droid
+{
+    [Activity(Label = "SplashActivity", MainLauncher = true, Theme = "@style/MyTheme.Splash", NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        protected override async void OnResume()
+        {
+            base.OnResume();
+            await SimulateStartup();
+
+        }
+
+        async Task SimulateStartup()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(4));
+                      
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+        }
+    }
+}
