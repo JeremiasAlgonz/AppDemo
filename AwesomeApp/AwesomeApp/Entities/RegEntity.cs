596b8b3765e3234597923827c537011a0a3c4e46
Jeremias Algonz <jeremiasgoncalez@gmail.com>
2022-02-26 22:46:42 -0300

    Implemented CRUD SQLITE & EntityTable

---
+++ AwesomeApp/AwesomeApp/Entities/RegEntity.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+
+namespace AwesomeApp.Entities
+{
+    public class RegEntity
+    {
+        public RegEntity()
+        {
+        }
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Phone { get; set; }
+        public char Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
